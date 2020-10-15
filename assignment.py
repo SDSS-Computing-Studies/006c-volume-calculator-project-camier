bdb79858a4bff3e2ca411a86ab187f4f48ba17a0
LemonskyChara <71406789+LemonskyChara@users.noreply.github.com>
2020-10-15 12:59:32 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -27,23 +27,28 @@ Instructions for use:
 
 """
 
+import math
 def title():
     # Will display a title screen
     # input parameters: none needed
     # output parameters: None
-    # Author:
+    # Author: Chara Yu
     # Modified:
     print("\n\n")
-    print("==================== Volumetric calculator ====================")
-    print("\n\n")
+    print("==================== Volumetric calculator ====================")                                  
+    for i in range(0,5):
+        print("*" * (5 - 1 - i) + " " * (i + 1) + "                                                      " + " " * i +"*" * (5 - 1 - i))
     return None
 
 def instructions():
     # Will display instructions
     # input parameters: none needed
     # output parameters: None
-    # Author:
+    # Author: Jacky Xu
     # Modified:
+    insbool = int(input("\nIf you want to see the instructions, please enter the number 1, otherwise, enter 0: \n"))
+    if insbool == 1:
+        print("Introduction:\n> This is a program for calculating the volume of different geometries.\n> You need to input variables namely geometry length and height. Then the program will calculate the geometry volume and display the result.\n> The geometry list:\n[ Cuboid , Cube , Cylinder , Cone(round) , Cone(sided) , Sphere ]\n> If you want to know more introduction and the principle of program operation, please check out the attachment [ Program description ].\nInstructions for use:\n1 - You need to choose one geometry from the geometry list.\n2 - You need to enter the data of the geometry which you want to calculate in the input box, and then press Enter.\n3 - The program automatically calculates the volume of the geometry and displays the results.\n4 - When you finish using the program, you can choose to exit. If you do not choose to exit, the system will automatically return to the original interface, allowing you to re-select the geometry calculation.")
     return None
 
 def getParams(shape):
@@ -52,9 +57,33 @@ def getParams(shape):
     # input parameter: string 
     # output parameter: return a list containing the prompts for each shape:
     # example: ["Enter the radius:","Enter the slant height:","Enter the height:"]
-    prompts
-
-    return prompts
+    if shape == 1:
+        global a,b,c
+        a = float(input("\nEnter the length: "))
+        b = float(input("\nEnter the width: "))
+        c = float(input("\nEnter the height: "))
+        return a,b,c
+    elif shape == 2:
+        a = float(input("\nEnter the length: "))
+        return a
+    elif shape == 3:
+        a = float(input("\nEnter the radius: "))
+        b = float(input("\nEnter the height: "))
+        return a,b
+    elif shape == 4:
+        a = float(input("\nEnter the bottom area: "))
+        b = float(input("\nEnter the height: "))
+        return a,b
+    elif shape == 5:
+        a = float(input("\nEnter the radius: "))
+        b = float(input("\nEnter the height: "))
+        return a,b
+    elif shape == 6:
+        a = float(input("\nEnter the radius: "))
+        return a
+    else:
+        print("\nWrong number.")
+        exit()
 
 def getInputs(questions):
     # Will prompt the user for inputs for the shape they.
@@ -62,15 +91,42 @@ def getInputs(questions):
     # It will turn all the input data into a list
     # input parameter: list containing the prompts/questions
     # output parameter: return a list containing all the measurements of the shape
-    measurements
+    if questions == 1:
+        global answer
+        answer = a * b * c
+        return answer 
+    elif questions == 2:
+        answer = a * a * a
+        return answer
+    elif questions == 3:
+        answer = math.pi * a ** 2 * b
+        return answer
+    elif questions == 4:
+        answer = 1 / 3 * a * b
+        return answer
+    elif questions == 5:
+        answer = 1 / 3 * math.pi * a ** 2 * b
+        return answer
+    elif questions == 6:
+        answer = 4 / 3 * math.pi * a ** 3
+        return answer
     
-    return measurements
 
 def main():
     # main block of code that will run your program and control program flow
     # You will need to include a while loop to keep repeating the commands until
     # the user chooses to exit
+    #1 chang fang ti 2:zheng fang ti 3:yuanzhu 4:jinzita 5:yuanzhuiti
     title()
+    exitbool = int(1)
+    while exitbool != 0:
+        instructions()
+        print("\n\nNumber 1 stands for cuboid\n\nNumber 2 stands for cube\n\nNumber 3 stands for cylindrical\n\nNumber 4 stands for pyramid\n\nNumber 5 stands for cone\n\nNumber 6 stands for sphere")
+        shapebool = int(input("\nChoose one number: "))
+        getParams(shapebool)
+        getInputs(shapebool)
+        print("\nThe answer is: " + str(answer))
+        exitbool = int(input("\nIf you want to stop it, enter 0 to stop, enter 1 to continue: "))
 
 
 main()
